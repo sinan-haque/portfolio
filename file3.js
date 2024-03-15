cf7848b74f626b50bd8f7e44d0fda0e17e9cd949
sinan-haque <145665893+sinan-haque@users.noreply.github.com>
2024-03-15 11:50:40 +0600

    Add files via upload

---
+++ file3.js
@@ -0,0 +1,9 @@
+function showList(){
+    const sidebar=document.querySelector('.sidebar');
+    sidebar.style.display='flex'
+}
+function hideList(){
+    const sidebar2=document.querySelector('.sidebar')
+    sidebar2.style.display='none';
+}
+console.log('hello world');
